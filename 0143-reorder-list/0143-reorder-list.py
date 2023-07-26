b83d158ee60141d387d2b8309b919ea2c1824f73
Mihir Arya <55593949+RGBmarya@users.noreply.github.com>
2023-07-26 16:54:51 -0700

    Time: 56 ms (99.88%), Space: 31.1 MB (45.18%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.py
@@ -0,0 +1,72 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    """
+    Understand
+    
+    1 2 3 4 
+    -> 1 4 2 3
+    
+    1 2 3 4 5
+    -> 1 5 2 4 3
+    
+    1 2 3 4 5 6
+    -> 1 6 2 5 3 4
+    
+    Match
+    Linked List
+    
+    Plan
+    - multi-pass, two-pointer
+    - first pass: slow pointer, fast pointer (2x) --> determine midpoint
+    - reverse right half of linked list
+    - second pass: left pointer at 0, right pointer at n
+    - while left != right:
+        - if from left:
+            - left -> right
+            - shift left
+            - change to right
+        - if from right:
+            - right -> left
+            - shift right
+            - change to left
+    - midpoint node.next = None
+    - return
+    """
+    def reorderList(self, head):
+        """
+        :type head: ListNode
+        :rtype: None Do not return anything, modify head in-place instead.
+        """
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        # slow is now at midpoint
+        
+        
+        curr = slow.next
+        prev = None
+        slow.next = None
+       # reverse right half of LL
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        
+        left = head
+        right = prev
+        
+        while right:
+            left_temp = left.next
+            right_temp = right.next
+            left.next = right
+            right.next = left_temp
+            left = left_temp
+            right = right_temp
+        
