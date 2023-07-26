ccbc3038ce2eb420d7aa5061dbf20f17bf61fe55
Mihir Arya <55593949+RGBmarya@users.noreply.github.com>
2023-07-25 23:27:17 -0700

    Time: 52 ms (69.06%), Space: 13.3 MB (93.87%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -0,0 +1,73 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    """
+    Understand
+    
+    1 2 3
+    4 5 6
+    -> 5 7 9
+    
+    7 3 5
+    4 2 3
+    -> 1 6 8
+    
+    0
+    0
+    -> 0
+    
+    3 4 5
+    2 4 7
+    -> 5 8 2 1
+    
+    2 3
+    9 9 3
+    -> 1 3 4
+    
+    Match
+    Linked list
+    
+    Plan
+    - dummyhead for result LL
+    - set pointers on both LL to head
+    - while either node is valid
+        - add values of nodes + add carry
+        - if one of nodes is None, replace with 0
+        - // 10 to determine if need to carry the 1 (could be 0 if <= 10)
+        - add node to dummyhead
+        - increment the dummyhead, l1, l2
+    - return 
+    """
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        dummy = ListNode(-1)
+        res = dummy
+        
+        carry = 0
+        while l1 or l2:
+            if not l1:
+                place_sum = l2.val
+                l2 = l2.next
+            elif not l2:
+                place_sum = l1.val
+                l1 = l1.next
+            else:
+                place_sum = l1.val + l2.val
+                l1 = l1.next
+                l2 = l2.next
+            place_sum += carry
+            carry = place_sum // 10
+            res.next = ListNode(place_sum % 10)
+            res = res.next
+
+        if carry == 1:
+            res.next = ListNode(1)
+        return dummy.next
+            
