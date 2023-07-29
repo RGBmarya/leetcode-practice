c18942c6012270c15f95883dac3cc8565ef853da
Mihir Arya <55593949+RGBmarya@users.noreply.github.com>
2023-07-29 14:47:09 -0700

    Time: 55 ms (90.77%), Space: 16.8 MB (65.11%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,4 +1,3 @@
-from collections import defaultdict
 
 """
 Understand
@@ -13,14 +12,17 @@ hashmap
 Plan
 - create hashmap for each word (letter: frq)
 - compare hashmaps
+
+Evaluate 
+Time: O(s + t)
+Space: O(s + t)
 """
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_hash = defaultdict(int)
-        t_hash = defaultdict(int)
+        s_hash, t_hash = {}, {}
         for l in s:
-            s_hash[l] += 1
+            s_hash[l] = 1 + s_hash.get(l, 0)
         for l in t:
-            t_hash[l] += 1
+            t_hash[l] = 1 + t_hash.get(l, 0)
         return s_hash == t_hash
