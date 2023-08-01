bda907087ca6bbbb8c863f69dc6b3841c71cd6d5
Mihir Arya <55593949+RGBmarya@users.noreply.github.com>
2023-08-01 13:20:53 -0700

    Time: 101 ms (95.52%), Space: 21.8 MB (28.98%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -20,16 +20,40 @@ Plan
 - iterate over the list to update frequencies in hashmap - O(n)
 - hashmap -> max heap with key = frq -> pop k times to return top k most frequent elements
 
-Evaluate
-Time: O(n) + O(n) + O(n) + O(k) -> O(n)
-Space: O(n) + O(n) -> O(n)
+
 """
 
 class Solution:
+    """
+    Evaluate
+    Time: O(n + n + klogn) = O(klogn)
+    Space: O(n) + O(n) -> O(n)
+    """
+    # def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    #     counts = {}
+    #     for num in nums:
+    #         counts[num] = 1 + counts.get(num, 0)
+    #     max_heap = [(f * -1, num) for num, f in counts.items()]
+    #     heapq.heapify(max_heap)
+    #     return [heapq.heappop(max_heap)[1] for i in range(k)]
+    
+    """
+    Evaluate
+    Time: O(n)
+    
+    """
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        counts = {}
-        for num in nums:
-            counts[num] = 1 + counts.get(num, 0)
-        max_heap = [(f * -1, num) for num, f in counts.items()]
-        heapq.heapify(max_heap)
-        return [heapq.heappop(max_heap)[1] for i in range(k)]
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        for n, c in count.items():
+            freq[c].append(n)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
