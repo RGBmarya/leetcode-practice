ae9c4f4a243e17f36403e387936179ed99ceb072
Mihir Arya <55593949+RGBmarya@users.noreply.github.com>
2023-08-16 15:22:20 -0700

    Time: 36 ms (91.15%), Space: 16.3 MB (25.14%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -6,7 +6,7 @@ Match
 recursion
 
 Plan
-base - if n == 1, return True; elif n == 0
+base - if n == 1, return True; elif n < 1, return False
 recursive case: func(n / 2)
 
 """
@@ -15,7 +15,7 @@ class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         if n == 1:
             return True
-        elif n < 1:
+        elif n % 2 != 0 or n < 1:
             return False
         
         return self.isPowerOfTwo(n / 2)
