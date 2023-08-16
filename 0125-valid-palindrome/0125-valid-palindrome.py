5dc235cf19365b01b20b7e054f92e2567503131a
Mihir Arya <55593949+RGBmarya@users.noreply.github.com>
2023-08-16 16:22:49 -0700

    Time: 47 ms (89.44%), Space: 17.2 MB (61.51%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -8,16 +8,26 @@ Plan
 - 2 pointers, 1 at each end --> iterate inwards while comparing values
 - if values differ, return False
 - return True
+
+Evaluate
+Time: O(n + n) -> O(n)
+Space: O(1)
 """
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = s.lower()
-        lst = [c for c in s if c.isalnum()]
-        l, r = 0, len(lst) - 1
-        while l <= r:
-            if lst[l] != lst[r]:
+        l, r = 0, len(s) - 1
+        
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while r > l and not s[r].isalnum():
+                r -= 1
+            
+            if s[l] != s[r]:
                 return False
+            
             l += 1
             r -= 1
         return True
