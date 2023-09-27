3bb12af78d0db53fec8d5f4a6e7aba9daecf4d30
Mihir Arya <msarya@icloud.com>
2023-09-27 01:49:03 -0400

    Time: 510 ms (84.63%), Space: 32.1 MB (26.29%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -4,7 +4,7 @@ Understand
 [2, 1, 3, 1] --> 1
 
 Match
-Linked list
+Hashmap
 
 Plan
 hashmap (num, frq)
@@ -13,13 +13,29 @@ return element with frq != 1 (max frq)
 XXXX - Floyd's tortoise and hare algorithm --> eventually pointers will intersect, which can result in false positive 
 
 Evalute
-Time: O(n)
+Time: O(n + n) --> O(n)
 Space: O(n - 1) --> O(n)
 """
 
 class Solution:
+#     def findDuplicate(self, nums: List[int]) -> int:
+#         counts = {}
+#         for num in nums:
+#             counts[num] = 1 + counts.get(num, 0)
+#         return max(counts, key=lambda n: counts.get(n))
+
+    """
+    Plan
+    iterate over list, keeping track of encountered elements in set
+    if encountered, return cur element
+    
+    Evaluate
+    Time: O(n)
+    Space: O(n)
+    """
     def findDuplicate(self, nums: List[int]) -> int:
-        counts = {}
+        encountered = set()
         for num in nums:
-            counts[num] = 1 + counts.get(num, 0)
-        return max(counts, key=lambda n: counts.get(n))
+            if num in encountered:
+                return num
+            encountered.add(num)
