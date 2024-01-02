8704a5e61b2f55ca920a203cc2f46bf29d75fe39
Mihir Arya <msarya@icloud.com>
2024-01-01 19:33:55 -0800

    Time: 44 ms (63.84%), Space: 18 MB (27.06%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -14,6 +14,9 @@ Plan
      - add current value to hashmap[level]
      - add children of current node to the deque
 
+Evaluate
+- Time: O(N)
+- Space: queue is largest at last level (~N/2 nodes) --> O(N/2) = O(N)
 """
 
 # Definition for a binary tree node.
@@ -23,19 +26,37 @@ Plan
 #         self.left = left
 #         self.right = right
 class Solution:
-    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root:
-            return []
+#     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+#         if not root:
+#             return []
     
-        hashmap = defaultdict(list)
-        queue = deque()
-        queue.append((0, root))
-        while queue:
-            cur_level, cur_node  = queue.popleft()
-            hashmap[cur_level].append(cur_node.val)
-            if cur_node.left:
-                queue.append((cur_level + 1, cur_node.left))
-            if cur_node.right:
-                queue.append((cur_level + 1, cur_node.right))
-        return hashmap.values()
+#         hashmap = defaultdict(list)
+#         queue = deque()
+#         queue.append((0, root))
+#         while queue:
+#             cur_level, cur_node  = queue.popleft()
+#             hashmap[cur_level].append(cur_node.val)
+#             if cur_node.left:
+#                 queue.append((cur_level + 1, cur_node.left))
+#             if cur_node.right:
+#                 queue.append((cur_level + 1, cur_node.right))
+#         return hashmap.values()
+    
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
+        q = deque()
+        if root:
+            q.append(root)
+        while q:
+            level = []
+            for i in range(len(q)): # only evaluates at creation of for loop 
+                node = q.popleft()
+                level.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            res.append(level)
+        return res
+                
             
