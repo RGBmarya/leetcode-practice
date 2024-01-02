23c5dd91751bdef378dc62953ed57ff3c06d62ba
Mihir Arya <msarya@icloud.com>
2024-01-01 19:21:00 -0800

    Time: 63 ms (6.29%), Space: 18 MB (21.85%) - LeetHub

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -0,0 +1,41 @@
+from collections import deque, defaultdict
+
+"""
+Understand
+
+Match
+BFS
+
+Plan
+- hashmap (level --> list of nodes)
+- create a deque (level, node)
+- while q is not empty:
+     - pop from left --> current node, level
+     - add current value to hashmap[level]
+     - add children of current node to the deque
+
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+    
+        hashmap = defaultdict(list)
+        queue = deque()
+        queue.append((0, root))
+        while queue:
+            cur_level, cur_node  = queue.popleft()
+            hashmap[cur_level].append(cur_node.val)
+            if cur_node.left:
+                queue.append((cur_level + 1, cur_node.left))
+            if cur_node.right:
+                queue.append((cur_level + 1, cur_node.right))
+        return hashmap.values()
+            
